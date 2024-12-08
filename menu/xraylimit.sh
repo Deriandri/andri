cb253f7fbc1230d287bf977357a3e135aa51ef81
Deriandri <deriandriananda82@gmail.com>
2024-12-08 15:08:09 +0700

    Update xraylimit.sh

--- menu/xraylimit.sh
+++ menu/xraylimit.sh
@@ -6,7 +6,7 @@ RED="\033[0;31m"
 COLOR1="$(cat /etc/rmbl/theme/$colornow | grep -w "TEXT" | cut -d: -f2|sed 's/ //g')"
 COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g')"
 WH='\033[1;37m'
-ipsaya=$(wget -qO- ipv4.icanhazip.com)
+ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
 data_ip="https://raw.githubusercontent.com/Sotgen/izin_new/refs/heads/main/ip"
@@ -34,7 +34,7 @@ bash2=$( pgrep bash | wc -l )
 if [[ $bash2 -gt "20" ]]; then
 killall bash
 fi
-inaIP=$(wget -qO- ipv4.icanhazip.com)
+inaIP=$(curl -sS ipv4.icanhazip.com)
 timenow=$(date +%T" WIB")
 TIMES="10"
 CHATID=$(cat /etc/perlogin/id)
@@ -126,11 +126,11 @@ else
 vmip=$(cat /etc/vmess/${vmuser}IP)
 fi
 if [[ ${vmhas} -gt "0" ]]; then
-downlink=$(xray api stats --server=127.0.0.1:10085 -name "user>>>${vmuser}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
+downlink=$(xray api stats --server=127.0.0.1:10000 -name "user>>>${vmuser}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
 cd
 if [ ! -e /etc/limit/vmess/${vmuser} ]; then
 echo "${downlink}" > /etc/limit/vmess/${vmuser}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${vmuser}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${vmuser}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 else
 plus2=$(cat /etc/limit/vmess/${vmuser})
 if [[ -z ${plus2} ]]; then
@@ -138,7 +138,7 @@ echo "1" > /etc/limit/vmess/${vmuser}
 fi
 plus3=$(( ${downlink} + ${plus2} ))
 echo "${plus3}" > /etc/limit/vmess/${vmuser}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${vmuser}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${vmuser}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 fi
 if [ ! -e /etc/vmess/${vmuser} ]; then
 echo "999999999999" > /etc/vmess/${vmuser}
@@ -309,11 +309,11 @@ else
 vlip=$(cat /etc/vless/${vlus}IP)
 fi
 if [[ ${vlsss} -gt "0" ]]; then
-downlink=$(xray api stats --server=127.0.0.1:10085 -name "user>>>${vlus}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
+downlink=$(xray api stats --server=127.0.0.1:10000 -name "user>>>${vlus}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
 cd
 if [ ! -e /etc/limit/vless/${vlus} ]; then
 echo "${downlink}" > /etc/limit/vless/${vlus}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${vlus}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${vlus}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 else
 plus2=$(cat /etc/limit/vless/${vlus})
 cd
@@ -322,7 +322,7 @@ echo "1" > /etc/limit/vless/${vlus}
 fi
 plus3=$(( ${downlink} + ${plus2} ))
 echo "${plus3}" > /etc/limit/vless/${vlus}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${vlus}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${vlus}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 fi
 cd
 if [ ! -e /etc/vless/${vlus} ]; then
@@ -495,11 +495,11 @@ else
 sadsde=$(cat /etc/trojan/${usrtr}IP)
 fi
 if [[ ${trip} -gt "0" ]]; then
-downlink=$(xray api stats --server=127.0.0.1:10085 -name "user>>>${usrtr}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
+downlink=$(xray api stats --server=127.0.0.1:10000 -name "user>>>${usrtr}>>>traffic>>>downlink" | grep -w "value" | awk '{print $2}' | cut -d '"' -f2)
 cd
 if [ ! -e /etc/limit/trojan/$usrtr ]; then
 echo "${downlink}" > /etc/limit/trojan/${usrtr}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${usrtr}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${usrtr}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 else
 plus2=$(cat /etc/limit/trojan/$usrtr)
 if [[ -z ${plus2} ]]; then
@@ -507,7 +507,7 @@ echo "1" > /etc/limit/trojan/$usrtr
 fi
 plus3=$(( ${downlink} + ${plus2} ))
 echo "${plus3}" > /etc/limit/trojan/${usrtr}
-xray api stats --server=127.0.0.1:10085 -name "user>>>${usrtr}>>>traffic>>>downlink" -reset > /dev/null 2>&1
+xray api stats --server=127.0.0.1:10000 -name "user>>>${usrtr}>>>traffic>>>downlink" -reset > /dev/null 2>&1
 fi
 if [ ! -e /etc/trojan/${usrtr} ]; then
 echo "999999999999" > /etc/trojan/${usrtr}
